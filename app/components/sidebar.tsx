90d4eaa56c78302699bc64e11caa92a3a64b88cd
Camilo <alejandro_noss@hotmail.com>
2023-04-21 22:10:06 -0500

    Sidebar template completed

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = ({ data }: SidebarProps) => {
   return (
     // Aside is a semantic HTML element that represents a section of a page, usually to represent a sidebar.
     <aside className="sidebar">
-      <div className="text-white flex flex-col p-10 items-center">
+      <div className="profile-container">
         <Image
           priority
           width={300}
@@ -23,25 +23,43 @@ const Sidebar = ({ data }: SidebarProps) => {
           alt="Profile image"
           className="profile-image"
         />
-        <h1>{name}</h1>
-        <h2>{role}</h2>
-        <p>{education[0]}</p>
-        <p>{education[1]}</p>
-        <p>{education[2]}</p>
-        <div>
-          <h3>Contact Me</h3>
-          <a href={contactLinks?.[0]} aria-label="Email link">
-            <Envelope />
-          </a>
-          <a href={contactLinks?.[1]} aria-label="Twitter link">
-            <Twitter />
-          </a>
-          <a href={contactLinks?.[2]} aria-label="LinkedIn link">
-            <Linkedin />
-          </a>
-          <a href={contactLinks?.[3]} aria-label="GitHub link">
-            <Github />
-          </a>
+        <h1 className="mb-2">{name}</h1>
+        <h2 className="mb-8">{role}</h2>
+        <p className="mb-2">{education[0]}</p>
+        <p className="mb-2">{education[1]}</p>
+        <p className="mb-2">{education[2]}</p>
+        <div className="contact-me-container ">
+          <h3>CONTACT ME</h3>
+          <nav>
+            <a
+              className="icons-contact-me"
+              href={contactLinks?.[0]}
+              aria-label="Email link"
+            >
+              <Envelope />
+            </a>
+            <a
+              className="icons-contact-me"
+              href={contactLinks?.[1]}
+              aria-label="Twitter link"
+            >
+              <Twitter />
+            </a>
+            <a
+              className="icons-contact-me"
+              href={contactLinks?.[2]}
+              aria-label="LinkedIn link"
+            >
+              <Linkedin />
+            </a>
+            <a
+              className="icons-contact-me"
+              href={contactLinks?.[3]}
+              aria-label="GitHub link"
+            >
+              <Github />
+            </a>
+          </nav>
         </div>
       </div>
     </aside>
