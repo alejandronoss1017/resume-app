684cfe08412950f161641e67bb5ce30fddf59616
Camilo <alejandro_noss@hotmail.com>
2023-04-21 23:03:44 -0500

    New about-me component

---
+++ app/components/about-me.tsx
@@ -0,0 +1,40 @@
+import React, { useId } from 'react';
+import { AboutMe } from '../data/types-data';
+import Skills from './skills';
+
+// We define the type of props that our component will receive
+type AboutMeProps = {
+  aboutMe: AboutMe;
+};
+
+const AboutMe = ({ aboutMe }: AboutMeProps) => {
+  const id = useId();
+  const { title, body } = aboutMe;
+
+  /**
+   * Map over the body array and return a paragraph for each item,
+   * using the index as the key.
+   *
+   * We use the useId hook to generate a unique id for the component, have
+   * to do this because we are using the same component multiple times on the page.
+   *
+   * This will render the body array as a list of paragraphs.
+   *
+   */
+  const bodyList: JSX.Element[] = body.map((item, i) => (
+    <p key={`${id}_${i}`} className="mb-6">
+      {item}
+    </p>
+  ));
+
+  return (
+    //Section is a semantic HTML element that represents a section of a page.
+    <section>
+      <h2 className="mb-8">{title}</h2>
+      {bodyList}
+      <Skills></Skills>
+    </section>
+  );
+};
+
+export default AboutMe;
