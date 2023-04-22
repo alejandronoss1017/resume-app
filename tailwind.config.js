8cf78010aed84edfa2b180985f082472b214487d
Camilo Nossa <alejandro_noss@hotmail.com>
2023-04-22 13:16:31 -0500

    New font sizes established

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,15 +21,15 @@ module.exports = {
       display: ['Oswald']
     },
     fontSize: {
-      sm: '0.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem'
+      sm: "0.8rem",
+			base: "1rem",
+			lg: "1.15rem",
+			xl: "1.25rem",
+			"2xl": "1.563rem",
+			"3xl": "1.953rem",
+			"4xl": "2.441rem",
+			"5xl": "3.052rem",
+			"6xl": "3.5rem",
     },
     extend: {
       screens: {
