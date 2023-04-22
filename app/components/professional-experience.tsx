2efce57d72d500a32210a1d6ff3f589a8098ca21
Camilo Nossa <alejandro_noss@hotmail.com>
2023-04-22 13:21:56 -0500

    New component to do

---
+++ app/components/professional-experience.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ProfessionalExperience = () => {
+  return (
+    <section>
+      <h2>Professional experience</h2>
+      <hr className="mb-6 border-2" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </section>
+  );
+};
+
+export default ProfessionalExperience;
