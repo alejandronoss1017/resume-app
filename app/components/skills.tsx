d612ebd7450498e194b6a6809ee3803ae8b2f2da
Camilo <alejandro_noss@hotmail.com>
2023-04-21 23:04:10 -0500

    New skills component

---
+++ app/components/skills.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <button type="button"> Hard Skills</button>
+      <button type="button" disabled={false}>
+        Soft Skills
+      </button>
+    </div>
+  );
+};
+
+export default Skills;
