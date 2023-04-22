e84b43f713a7226e05e2436de3a0c0ed06f5ec6b
Camilo Nossa <alejandro_noss@hotmail.com>
2023-04-22 13:22:50 -0500

    ProfessionalExperience added to be render

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutMe from './components/about-me';
 import { aboutMe, skills, professional } from './data/page-data';
+import ProfessionalExperience from './components/professional-experience';
 
 //Here should be the code for main page, the part next to the aside
 export default function Home() {
   return (
-    <>
+    <div>
       <AboutMe aboutMe={aboutMe}></AboutMe>
-    </>
+      <ProfessionalExperience />
+    </div>
   );
 }
