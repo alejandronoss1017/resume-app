2c06202df58c517cfb607751b69764f62ebbd890
Camilo Nossa <calejandro_noss@outlook.com>
2023-07-01 00:10:22 -0500

    Finished professional-experience component

--- app/components/professional-experience.tsx
+++ app/components/professional-experience.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Professional } from '../data/types-data';
 
-const ProfessionalExperience = () => {
+type ProfessionalExperienceProps = {
+  professional: Professional;
+};
+
+const ProfessionalExperience = ({
+  professional
+}: ProfessionalExperienceProps) => {
+  const { title, experiences } = professional;
   return (
     <section>
-      <h2>Professional experience</h2>
+      <h2>{title}</h2>
       <hr className="mb-6 border-2" />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      {experiences.map((experience, i) => (
+        <div key={i} className="mb-6">
+          <h3 className="mb-2">{experience.role}</h3>
+            <ul>
+              {experience.description.map((item, i) => (
+                <li key={i} className="mb-2 list-inside text-justify">
+                  {item}
+                </li>
+              ))}
+            </ul>
+        </div>
+      ))}
     </section>
   );
 };
