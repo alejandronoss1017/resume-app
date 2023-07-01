f5a66b42d18f922d27eca749b883a51689e020c9
Camilo Nossa <calejandro_noss@outlook.com>
2023-07-01 00:11:54 -0500

    Added components to render

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import AboutMe from './components/about-me';
-import { aboutMe, skills, professional } from './data/page-data';
+import { aboutMe, professional, projects } from './data/page-data';
 import ProfessionalExperience from './components/professional-experience';
+import Projects from './components/projects';
 
 //Here should be the code for main page, the part next to the aside
 export default function Home() {
   return (
     <div>
       <AboutMe aboutMe={aboutMe}></AboutMe>
-      <ProfessionalExperience />
+      <ProfessionalExperience professional={professional} />
+      <Projects projects={projects} />
     </div>
   );
 }
